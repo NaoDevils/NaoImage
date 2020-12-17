09b57b4d3ff2f341ad9dc0436575eb0839eca6a3
Aaron Larisch <aaron-github@openlarry.de>
2020-12-17 12:55:43 +0100

    Fix uid and gid

--- snippets/opencl.sh
+++ snippets/opencl.sh
@@ -21,7 +21,7 @@ umount ./root/proc
 
 # install kernel with intel graphics driver
 tar -C ./root/boot -xvzpf kernel/kernel-4.4.86-rt99-aldebaran-g085ca88f9bd1.tgz
-tar -C ./root -xvzpf kernel/modules-4.4.86-rt99-aldebaran-g085ca88f9bd1.tgz
+tar -C ./root/usr -xvzpf kernel/modules-4.4.86-rt99-aldebaran-g085ca88f9bd1.tgz
 ln -sf vmlinuz-4.4.86-rt99-aldebaran-g085ca88f9bd1 ./root/boot/vmlinuz.efi
 ln -sf vmlinuz-4.4.86-rt99-aldebaran-g085ca88f9bd1 ./root/boot/vmlinuz
 # tar -C ./root/boot -xvzpf kernel/kernel-5.4.70-rt40-aldebaran-naodevils-00371-g2f8f53592324.tgz
