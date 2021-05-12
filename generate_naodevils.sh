2d0bacaf5d5c6d415e9779b4bb05fd1ed3b20610
Aaron Larisch <aaron.larisch@tu-dortmund.de>
2021-05-12 11:22:51 +0000

    Add naodevils generate script

---
+++ generate_naodevils.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [ "$#" -eq 0 ]; then
+    echo "Illegal number of parameters"
+    echo "Usage: $0 nao-image.opn [root@naoip]"
+    exit 1
+fi
+
+INPUT_OPN="$1"
+SSH="$2"
+
+export FRAMEWORK_DIR=../NDevils2015/
+export COPY_DATA=true
+export DHCP=false
+
+GIT_IMAGE="$(git rev-parse --short HEAD)"
+GIT_FRAMEWORK="$(git -C "$FRAMEWORK_DIR" rev-parse --short HEAD)"
+DATE="$(date +%Y%m%d_%H%M%S)"
+
+./generate_image.sh "$INPUT_OPN" "image.ext3" opencl naodevils-framework naodevils-robotconfig
+./generate_opn.sh "image.ext3" "naodevils_${DATE}_${GIT_FRAMEWORK}_${GIT_IMAGE}.opn"
+
+if [ "$#" -eq 2 ]; then
+    ./flash_robot.sh "naodevils_${DATE}_${GIT_FRAMEWORK}_${GIT_IMAGE}.opn" "$SSH"
+fi
