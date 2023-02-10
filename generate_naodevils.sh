c1b3ca8f97ecd92be93bb34e4ac3748911033a2e
Aaron Larisch <aaron.larisch@tu-dortmund.de>
2023-02-10 09:51:55 +0100

    Add config parameters to generate script

--- generate_naodevils.sh
+++ generate_naodevils.sh
@@ -3,25 +3,47 @@
 set -e
 set -o pipefail
 
-if [ "$#" -eq 0 ]; then
-    echo "Illegal number of parameters"
-    echo "Usage: $0 nao-image.opn [root@naoip]"
-    exit 1
+usage() { echo "Usage: $0 -i <nao-image.opn> [-f <framework directory>] [-r <root@naoip>]" 1>&2; exit 1; }
+
+while getopts "i:f:r:" o; do
+    case "${o}" in
+        i)
+            INPUT_OPN="${OPTARG}"
+            ;;
+        f)
+            FRAMEWORK_DIR="${OPTARG%/}"
+            ;;
+        r)
+            SSH="${OPTARG}"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+if [ -z "$INPUT_OPN" ]; then
+	usage
 fi
 
-INPUT_OPN="$1"
-SSH="$2"
+if [ -z "$FRAMEWORK_DIR" ]; then
+	rm -rf ./root.tgz
+	./generate_image.sh "$INPUT_OPN" "image.ext3" ubuntu naodevils-framework-base save-base
+	
+	GIT_FRAMEWORK="baseonly"
+else
+	export FRAMEWORK_DIR="$FRAMEWORK_DIR"
+	export DHCP=false
 
-export FRAMEWORK_DIR=../NDevils2015/
-export DHCP=false
-
-./generate_image.sh "$INPUT_OPN" "image.ext3" ubuntu naodevils-framework-base save-base naodevils-robotconfig naodevils-framework-copy
+	./generate_image.sh "$INPUT_OPN" "image.ext3" ubuntu naodevils-framework-base save-base naodevils-robotconfig naodevils-framework-copy
+	
+	GIT_FRAMEWORK="$(git -C "$FRAMEWORK_DIR" rev-parse --short HEAD)"
+fi
 
 GIT_IMAGE="$(git rev-parse --short HEAD)"
-GIT_FRAMEWORK="$(git -C "$FRAMEWORK_DIR" rev-parse --short HEAD)"
 DATE="$(date +%Y%m%d_%H%M%S)"
 ./generate_opn.sh "image.ext3" "naodevils_${DATE}_${GIT_FRAMEWORK}_${GIT_IMAGE}.opn"
 
-if [ "$#" -eq 2 ]; then
+if ! [ -z "$SSH" ]; then
     ./flash_robot.sh "naodevils_${DATE}_${GIT_FRAMEWORK}_${GIT_IMAGE}.opn" "$SSH"
 fi
